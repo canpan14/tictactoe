ec19c05a31168a28167f758cacebbb0c2927f0fd
canpan14 <sammachlin@hotmail.com>
2017-09-26 15:26:02 -0400

    Add gameboard changes

---
+++ assets/scripts/tictactoe/gameBoard.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const boardArray = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+const makeMove = function (player, row, col) {
+  boardArray[row][col] = player
+  return checkForWin()
+}
+
+// Checks for win based on last play
+const checkForWin = function () {
+  if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin()) {
+    return true
+  }
+  return false
+}
+
+const checkHorizontalWin = function () {
+  for (let i = 0; i < boardArray.length; i++) {
+    if (isArrayAllSameValues(boardArray[i])) {
+      return true
+    }
+  }
+  return false
+}
+
+const checkVerticalWin = function (c) {
+  for (let i = 0; i < boardArray[0].length; i++) {
+    if (isArrayAllSameValues([boardArray[0][i], boardArray[1][i], boardArray[2][i]])) {
+      return true
+    }
+  }
+  return false
+}
+
+const checkDiagonalWin = function (r, c) {
+  return checkDownLeft() || checkDownRight()
+}
+
+const checkDownRight = function () {
+  if (isArrayAllSameValues([boardArray[0][0], boardArray[1][1], boardArray[2][2]])) {
+    return true
+  }
+  return false
+}
+
+const checkDownLeft = function () {
+  if (isArrayAllSameValues([boardArray[0][2], boardArray[1][1], boardArray[2][0]])) {
+    return true
+  }
+  return false
+}
+
+const isArrayAllSameValues = function (arr) {
+  return arr.every(ele => ele === arr[0])
+}
+
+module.exports = {
+  makeMove,
+  checkForWin
+}
