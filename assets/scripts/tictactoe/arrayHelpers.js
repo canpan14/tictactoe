95a5f951e1151d51139adb5156a36d9ac5725726
canpan14 <sammachlin@hotmail.com>
2017-09-27 09:57:37 -0400

    Documented arrayHelpers

--- assets/scripts/tictactoe/arrayHelpers.js
+++ assets/scripts/tictactoe/arrayHelpers.js
@@ -1,14 +1,22 @@
 'use strict'
 
 // https://stackoverflow.com/questions/22464605/convert-a-1d-array-to-2d-array
-// Takes in 1D array returns new 2D array (9 length to 3 by 3)
+/**
+ * Takes in 1D (Divisible by 3) array and returns a 2D array (3 by 3)
+ * @param  {array} arr 1D array with length 9
+ * @return {array}     2D array with dimensions 3 by 3
+ */
 const convertArray1Dto2D = function (arr) {
   const newArr = []
   while (arr.length) newArr.push(arr.splice(0, 3))
   return newArr
 }
 
-// Takes in 2D array returns 1D array (3 by 3 to 9 length)
+/**
+ * Takes in 2D (3 by 3) array and returns a 1D array (length 9)
+ * @param  {array} arr 2D array with dimensions 3 by 3
+ * @return {array}     1D array with length 9
+ */
 const convertArray2Dto1D = function (arr) {
   const newArr = arr.reduce((a, b) => {
     a.concat(b)
