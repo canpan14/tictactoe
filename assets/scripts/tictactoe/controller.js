9b1b74749fdf47dea8cab27ebc53466d30874425
canpan14 <sammachlin@hotmail.com>
2017-09-27 09:43:42 -0400

    Refactored and documented controller

--- assets/scripts/tictactoe/controller.js
+++ assets/scripts/tictactoe/controller.js
@@ -10,38 +10,94 @@ let currentPlayer
 let gameOver = false
 let turnCounter = 0
 
-// Creates initial players
+// External functions
+/**
+ * Initializes the game state on page load
+ * @return {undefined}
+ */
+const initializeGame = function () {
+  createPlayers()
+  changeTurns()
+}
+
+/**
+ * Main flow of the game that runs after each board click
+ * @param  {event} event Click event from clicking on the game board
+ * @return {undefined}
+ */
+const takeTurn = function (event) {
+  if (!isLegalMove(event)) return // Do nothing if illegal move
+  makeMove(event) // Make the move on the board
+  analyzeBoardState() // Check the new board for a win/draw and handle it
+}
+
+// Interal functions
+/**
+ * Creates the initial players
+ * @return {undefined}
+ */
 const createPlayers = function () {
   players.push(Player.createPlayer(-1, 'Player 1', 'X'))
   players.push(Player.createPlayer(1, 'Player 2', 'O'))
 }
 
-// Swaps player turns
+/**
+ * Handles a turn change
+ * @return {undefined}
+ */
 const changeTurns = function () {
   turnCounter++
   currentPlayer = turnCounter % 2 !== 0 ? players[0] : players[1]
   ui.onTurnChange(currentPlayer)
 }
 
-// What to do on a win
+/**
+ * Handles a win
+ * @return {undefined}
+ */
 const win = function () {
   $('#playerTurnText').text(currentPlayer.name + ' wins!')
 }
 
-// What to do on a draw
+/**
+ * Handles a draw
+ * @return {undefined}
+ */
 const draw = function () {
   $('#playerTurnText').text('Draw!')
 }
 
-// Main game flow
-// Update clicked cell
-const takeTurn = function (event) {
-  if (!isLegalMove(event)) return
-  ui.updateCell(event.target, currentPlayer.boardMarker)
+/**
+ * Checks if an attempted move is legal
+ * @param  {event} event The click event on the board
+ * @return {boolean} Return true if move is legal
+ */
+const isLegalMove = function (event) {
+  // If gameover or cell is already filled, ignore
+  if (gameOver || event.target.innerHTML) {
+    return false
+  }
+  return true
+}
+
+/**
+ * Makes the move on the board updating the visual and internal board
+ * @param  {event} event The click event on the board
+ * @return {undefined}
+ */
+const makeMove = function (event) {
+  ui.updateCell(event.target, currentPlayer.boardMarker) // Update the cell visually
   const cellClicked = event.target.attributes['data-pos'].value.split(',')
   const r = parseInt(cellClicked[0])
   const c = parseInt(cellClicked[1])
   gameBoard.makeMove(currentPlayer, r, c)
+}
+
+/**
+ * Checks the board for a win or draw and handles it
+ * @return {undefined}
+ */
+const analyzeBoardState = function () {
   if (!gameBoard.checkForWin()) {
     if (turnCounter >= 9) {
       gameOver = true
@@ -55,19 +111,7 @@ const takeTurn = function (event) {
   }
 }
 
-const isLegalMove = function (event) {
-  // If gameover or cell is already filled, ignore
-  if (gameOver || event.target.innerHTML) {
-    return false
-  }
-  return true
-}
-
-const initializeGame = function () {
-  createPlayers()
-  changeTurns()
-}
-
+// Exports
 module.exports = {
   initializeGame,
   takeTurn
